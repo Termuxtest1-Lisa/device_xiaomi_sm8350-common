8dc384d4a5adda9475d0a0aece2cdc55b446db4f
kubersharma001 <kubersharma001@gmail.com>
2021-08-27 17:25:10 +0000

    sm8350-common: Import powerhint.json from coral
    * From: https://android.googlesource.com/device/google/coral/+/android-11.0.0_r42/powerhint.json
    
    Change-Id: I8dba2912afb27e0432bdce601ce38f2934bb0186

--- common.mk
+++ common.mk
@@ -370,7 +370,8 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service.xiaomi-libperfmgr
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json \
+    $(LOCAL_PATH)/configs/perf/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 $(call soong_config_set,qtipower,mode_ext_lib,//$(LOCAL_PATH):libpowermode-ext-xiaomi)
 
