2ad95d3a7d8b089df73c4d3c3a674fb399d1c507
mikeNG <mikeNG@users.noreply.github.com>
2025-10-04 14:33:27 +0530

    sm8350-common: Add Config.xml From Hexdare Common trees and Enable WFD
    sm8350-common: Add Config.xml From Hexdare Common trees and Enable WFD

--- common.mk
+++ common.mk
@@ -317,6 +317,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
 # Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
+
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
