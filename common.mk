e11ebe50358bdb5341707bf10f27ca0b1493b4e1
Termux <Harshjangrap@gmail.com>
2026-01-30 21:23:39 +0530

    sm8350-common: Remove packages at build time
    Signed-off-by: 0mar99 <omarag9099@gmail.com>

--- common.mk
+++ common.mk
@@ -391,6 +391,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti.vendor # Needed by CNE app
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors-service.xiaomi-multihal
